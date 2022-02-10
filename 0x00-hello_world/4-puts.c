380e628cd85d8c5ccdaf7e62a7032c973a4679ac
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-10 11:14:26 -0500

    documentation and sintax corrections 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ *void 
+ *always return 0
+ */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("Programming is like building a multilingual puzzle%c", "\n");
 	return (0);
 }
 
