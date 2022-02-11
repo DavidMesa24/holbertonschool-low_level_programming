1e68e5b1fcd77a1f0a52a247769d224c3ef894a6
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-11 16:57:52 -0500

    two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - return alphabeth lowecase
+ *
+ * Return: zero always
+ */
+int main (void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar (l);
+		l++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
