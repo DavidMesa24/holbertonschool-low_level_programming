b8d66115086466ebdb6ff7bf04d47435d67b48d9
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-11 17:06:22 -0500

    three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - alphabeth lower case and upper case
+ *
+ * Return: zero always
+ */
+int main (void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
