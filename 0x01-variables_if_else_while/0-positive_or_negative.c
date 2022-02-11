cca4130e18cf5dc3c39af9c35ee2b2f433eb8e29
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-11 14:10:25 -0500

    postive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - return positive or negative
+ *
+ * Return: zero if n =0, positive if n>0 and negative if n<0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0)
+	{
+		printf("is positive\n");
+	}
+	else if(n==0)
+	{
+		ptintf("is zero\n");
+	}
+	else
+	{
+		printf("is negative \n");
+	}
+
+	return (0);
+}
