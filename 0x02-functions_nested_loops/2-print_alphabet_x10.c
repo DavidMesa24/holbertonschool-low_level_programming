6fbfdd2ba335deeb191e8982146c26ce4b36b13c
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-15 09:55:06 -0500

    two betty please

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
-/**print_alphabet_x10 - pues lo mprime x10 que mas jeje salu2
+/**
+ * print_alphabet_x10 - pues lo mprime x10 que mas jeje salu2
  *
  * Return : void
  */
 void print_alphabet_x10(void)
 {
-	char c; 
+	char c;
 	int i;
 
-	for (i = 0; i<10; i++)
-	{	
-		for (c = 'a'; c<= 'z'; c++)
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
