6b1679c68e02426cf2bba5dc1559e266d55f225a
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-16 17:49:08 -0500

    sfd

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int mayor;
 
-	if (a >= b && b >= c)
+	if (a > b && b > c)
 	{
 		mayor = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 	{
 		mayor = b;
 	}
@@ -27,4 +27,3 @@ int largest_number(int a, int b, int c)
 
 	return (mayor);
 }
-
