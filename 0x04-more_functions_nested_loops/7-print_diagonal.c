1a69399f9c68ebdca224938206de1144c10b4b34
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-17 18:53:51 -0500

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_diagonal - pues ajá mor, el nombre lo dice perris
+ *@n: el numero de lineas
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == 0)
+		{
+			_putchar(98);
+		}
+		else
+		{
+			int espacios = n - 1;
+
+			while (espacios > 0)
+			{
+				_putchar(' ');
+				espacios--;
+			}
+			_putchar(98);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
