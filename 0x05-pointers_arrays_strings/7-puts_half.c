c36143d7ce8801cebe25643ab34b27e873435698
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-21 23:13:24 -0500

    seven betty fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,15 @@
 #include "main.h"
+/**
+ * _str len - length
+ * @s: char
+ * Return: void
+ */
 
 int _strlen(char *s)
 {
 
 	int length = 0;
+
 	while (*s != '\0')
 	{
 		length++;
@@ -11,25 +17,32 @@ int _strlen(char *s)
 	}
 	return (length);
 }
-
+/**
+ * puts_half
+ * @str - str
+ * Return: void
+ */
 void puts_half(char *str)
 {
 	int length_of_the_string = _strlen(str);
-	if ( length_of_the_string % 2 == 0)
+
+	if (length_of_the_string % 2 == 0)
 	{
 		int medio = length_of_the_string / 2;
+
 		while (medio < length_of_the_string)
 		{
 			_putchar(str[medio]);
 			medio++;
 		}
 	}
-	else 
+	else
 	{
 		int medio = (length_of_the_string - 1) / 2;
+
 		while (medio <	length_of_the_string - 1)
 		{
-			_putchar(str[medio+1]);
+			_putchar(str[medio + 1]);
 			medio++;
 		}
 	}
