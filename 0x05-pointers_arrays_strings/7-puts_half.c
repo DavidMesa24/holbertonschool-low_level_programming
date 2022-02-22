cdfbfe5cf1136d3c666332baa4e8d0f47680a1f8
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-21 23:02:08 -0500

    seven update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,18 +18,19 @@ void puts_half(char *str)
 	if ( length_of_the_string % 2 == 0)
 	{
 		int medio = length_of_the_string / 2;
-		while (medio < length_of_the_string)
+		while (medio <= length_of_the_string)
 		{
-			_putchar(str[medio - 1]);
-			medio ++;
+			_putchar(str[medio]);
+			medio++;
 		}
 	}
 	else 
 	{
 		int medio = (length_of_the_string - 1) / 2;
-		while (medio < length_of_the_string)
+		while (medio <= length_of_the_string)
 		{
-			_putchar(str[medio-1]);
+			_putchar(str[medio]);
+			medio++;
 		}
 	}
 	_putchar ('\n');
