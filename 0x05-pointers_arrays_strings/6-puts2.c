53449d66301ac705fe0ce42918af4f4f8a85ff4f
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-21 22:43:24 -0500

    6 first stage

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,35 @@
 #include "main.h"
 #include<stdbool.h>
+/**
+ * _strlen - return length
+ * @s: str
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+/**
+ * puts2 - print 2
+ * @str: str
+ * Return: void
+ */
 
 void puts2(char *str)
 {
 	bool aux = true;
 
 	int i = 0;
+	int j = _strlen(str);
 
-	while (*str != '\0')
+	while (i < j)
 	{
 		if (aux)
 		{
