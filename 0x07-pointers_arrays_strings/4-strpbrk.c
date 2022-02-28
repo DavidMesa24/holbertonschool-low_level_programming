46249117406395c9932a2b2d5fb0b8fa8b8e4d75
DavidMesa24git add .git commit -m h <davidmesa02@gmail.com>
2022-02-28 18:23:38 -0500

    four

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -17,22 +17,11 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
-			{		
+			{
 				return (s + i);
 			}
-			
 		}
 	}
 	return ('\0');
 }
 
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
-}
